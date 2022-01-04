8f6057ce008033bb8b7ab342b11be1bf44118b0f
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-01-04 05:19:38 +0100

    added support for 3 custom pages

---
+++ public/wp-content/themes/nina-mueller/page-about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+    <?php include 'head.php';?>
+    <body>
+        <?php wp_body_open();?>
+        <?php get_header(); ?>
+        <main class="min-main-height">
+            <div class="max-main-width">
+                <section class="container">
+                    <h2>This is a custom about page</h2>
+                </section>
+            </div>
+        </main>
+        <?php include 'footer.php';?>
+    </body>
+    <script src="<?php echo get_template_directory_uri().'/js/script.js' ?>"></script>
+</html>
