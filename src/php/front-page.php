09fe41852cc78b0906d457f888e54da8c3ceb463
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-01-21 22:23:59 +0100

    fixed issue with custom function

--- src/php/front-page.php
+++ src/php/front-page.php
@@ -39,25 +39,12 @@
                     $context['content'] = $updates_page[1];
                     Timber::render( 'intro.twig', $context );
                 ?>
-                <div class="container__posts">
-                    <?php
-                        /* using Normal WP Loop instead of Timber due to issues in Timber working with our Custon Function "get_yt_video_thumbnail"*/
-                        $services_query = new WP_Query('order=DESC&category_name=videos&posts_per_page=2');
-                        if ($services_query->have_posts()):
-                        while ($services_query->have_posts()): $services_query->the_post();?>
-                            <div class="container__posts__item">
-                                <img class="container__posts__item__image" src="<?php echo get_yt_video_thumbnail(get_field("yt_url")); ?>" alt="youtube thumbnail image" width="320" height="180">
-                                <a class="container__posts__item__overlay" href="<?php the_permalink();?>" target="_blank">
-                                    <img class="container__posts__item__overlay__button" src="<?php echo get_template_directory_uri().'/images/play-button.svg' ?>" alt="video play button" width="32" height="32">
-                                    <h4 class="container__posts__item__overlay__link"><?php the_title();?></h4>
-                                </a>
-                            </div>
-                        <?php endwhile;?>
-                        <?php else: ?>
-                    <p>Erster Post demnächst...</p>
-                    <?php endif;?>
-                    <?php wp_reset_postdata();?>
-                </div>
+                <?php 
+                    $context = Timber::context();
+                    $args = 'order=DESC&category_name=videos&posts_per_page=2';
+                    $context['posts'] = Timber::get_posts( $args );
+                    Timber::render( 'videos.twig', $context );
+                ?>
 
                 <a href="<?php echo get_permalink( get_page_by_path( $page ) ); ?>" target="_blank" class="redirect-arrow container__link"><h4>more videos</h4><i></i></a>
             </section>
--- src/php/page-videos.php
+++ src/php/page-videos.php
@@ -17,25 +17,12 @@
                     $context['content'] = $updates_page[1];
                     Timber::render( 'intro.twig', $context );
                 ?>
-                <div class="container__posts">
-                    <?php
-                        /* using Normal WP Loop instead of Timber due to issues in Timber working with our Custon Function "get_yt_video_thumbnail"*/
-                        $services_query = new WP_Query('order=DESC&category_name=videos');
-                        if ($services_query->have_posts()):
-                        while ($services_query->have_posts()): $services_query->the_post();?>
-                            <div class="container__posts__item">
-                                <img class="container__posts__item__image" src="<?php echo get_yt_video_thumbnail(get_field("yt_url")); ?>" alt="youtube thumbnail image" width="320" height="180">
-                                <a class="container__posts__item__overlay" href="<?php the_permalink();?>" target="_blank">
-                                    <img class="container__posts__item__overlay__button" src="<?php echo get_template_directory_uri().'/images/play-button.svg' ?>" alt="video play button" width="32" height="32">
-                                    <h4 class="container__posts__item__overlay__link"><?php the_title();?></h4>
-                                </a>
-                            </div>
-                        <?php endwhile;?>
-                        <?php else: ?>
-                    <p>Erster Post demnächst...</p>
-                    <?php endif;?>
-                    <?php wp_reset_postdata();?>
-                </div>
+                <?php 
+                    $context = Timber::context();
+                    $args = 'order=DESC&category_name=videos';
+                    $context['posts'] = Timber::get_posts( $args );
+                    Timber::render( 'videos.twig', $context );
+                ?>
             </section>
             </div>
         </main>
